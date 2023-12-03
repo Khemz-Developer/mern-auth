1b167141cd65236517ea341c88ce4de03c869b45
Khemz-Developer <jalithapramod@gmail.com>
2023-12-04 00:34:09 +0530

    signup complete

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="max-w-lg p-3 mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="p-3 rounded-lg bg-slate-100"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="p-3 rounded-lg bg-slate-100"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="p-3 rounded-lg bg-slate-100"
+        />
+
+        <button className="p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80">
+          signup
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-500'>Sign in</span>
+        </Link>
+      </div>
+     
+    </div>
+  
+  );
 }
