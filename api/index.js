dc525d141038fc2aea840b9cb7cb5c280da6f4ff
Khemz-Developer <jalithapramod@gmail.com>
2023-12-03 20:10:04 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+//password and name for database:mern-auth
+// mongoose.connect(process.env.MONGO).then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log('connected to MongoDb');
+}).catch((err) =>{
+  console.log(err);
+});
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('Server listening on port 3000');
+app.listen(4000, ()=>{
+    console.log('Server listening on port 4000');
 });
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>Ab0out</div>
   )
 }
