c267a4c22360af8dff1445c4965c5429994bb351
Khemz-Developer <jalithapramod@gmail.com>
2023-12-03 21:00:45 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 //password and name for database:mern-auth
 // mongoose.connect(process.env.MONGO).then(() => {
@@ -17,6 +18,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 
-app.listen(4000, ()=>{
-    console.log('Server listening on port 4000');
+app.listen(3000, ()=>{
+    console.log('Server listening on port 3000');
 });
+
+app.use("/api/user",userRoutes);
